43b8fb078fefde3bd01eca81c71b92fea766d85c
JaeCheon <studyJcHub@gmail.com>
2022-02-16 17:48:08 +0900

    과일 클래스 추가 및 주석 설명

--- app/src/main/java/com/jc/reviewlistview_20220216/datas/FruitData.kt
+++ app/src/main/java/com/jc/reviewlistview_20220216/datas/FruitData.kt
@@ -1,5 +1,10 @@
 package com.jc.reviewlistview_20220216.datas
 
+/**
+ * @param name 과일 이름
+ * @param price 과일 가격
+ * @param expirationDate 유통기한
+ */
 class FruitData(
     val name: String,
     val price: Int,
